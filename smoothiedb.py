46760f3c4ca6c019bc14494adee63b91f0f6a2a4
barrycunningham <cunnbarry@gmail.com>
2019-03-14 12:28:19 +0000

    First function written and connection to MongoDB established successfully

--- smoothiedb.py
+++ smoothiedb.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'smoothiedb'
+app.config["MONGO_URI"] = 'mongodb+srv://barra:barra00User@myfirstcluster-gjsrn.mongodb.net/smoothiedb?retryWrites=true'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello World Smoothie'
+@app.route('/get_smoothies')
+def get_smoothies():
+    return render_template("smoothies.html", smoothie_recipes=mongo.db.smoothie_recipes.find())
+   
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
