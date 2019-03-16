666d891f1b0f0a42508454848989bcd2fc20ec5b
barrycunningham <cunnbarry@gmail.com>
2019-03-16 14:56:53 +0000

    DB schema adjusted. get_smoothies connected and rendering acceptably.

--- smoothiedb.py
+++ smoothiedb.py
@@ -3,23 +3,26 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-# app.config["MONGO_DBNAME"] = 'smoothiedb'
-# app.config["MONGO_URI"] = 'mongodb+srv://barra:barra00User@myfirstcluster-gjsrn.mongodb.net/smoothiedb?retryWrites=true'
 
 mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_smoothies')
 def get_smoothies():
     return render_template("smoothies.html", smoothie_recipes=mongo.db.smoothie_recipes.find())
    
+
+@app.route('/add_smoothie')
+def add_smoothie():
+    return render_template('addsmoothie.html')
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
