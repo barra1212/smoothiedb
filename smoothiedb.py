33ea81c5e8f22a078155bbc7e0d872dda6cdf0bc
barrycunningham <cunnbarry@gmail.com>
2019-03-18 18:16:42 +0000

    Categories functionality added. Some basic styling added

--- smoothiedb.py
+++ smoothiedb.py
@@ -21,9 +21,55 @@ def get_smoothies():
 
 @app.route('/add_smoothie')
 def add_smoothie():
-    return render_template('addsmoothie.html')
+    return render_template('addsmoothie.html',
+    categories=mongo.db.categories.find())
+
+
+@app.route('/insert_smoothie', methods=['POST'])
+def insert_smoothie():
+    smoothie_recipes = mongo.db.smoothie_recipes
+    smoothie_recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_smoothies'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/delete_category/<category_id>', methods=['POST'])
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
     
-    
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
