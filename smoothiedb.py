0f22bc9e1d2aa3327b9e08452bfc7e5cd45d7ad2
barrycunningham <cunnbarry@gmail.com>
2019-04-01 16:33:16 +0000

    Social icons added to footer

--- smoothiedb.py
+++ smoothiedb.py
@@ -14,28 +14,25 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 
+# VARIABLES
+smoothie_recipes=mongo.db.smoothie_recipes
+categories=mongo.db.categories
+
+
 @app.route('/')
 @app.route('/get_smoothies')
 def get_smoothies():
-    return render_template("smoothies.html", smoothie_recipes=mongo.db.smoothie_recipes.find())
+    return render_template("smoothies.html", smoothie_recipes=smoothie_recipes.find())
    
-
-# @app.route('/search')
-# def search():
-#     smoothie_recipes = mongo.db.smoothie_recipes
-#     smoothie_recipes = smoothie_recipes.create_index([("$**", 'text')])
-#     return render_template("search-results.html", smoothie_recipes.find({"$text": {"$search": str('banana')}})
-
 
 @app.route('/add_smoothie')
 def add_smoothie():
     return render_template('addsmoothie.html',
-    categories=mongo.db.categories.find())
+    categories=categories.find())
     
 
 @app.route('/insert_smoothie', methods=['POST'])
 def insert_smoothie():
-    smoothie_recipes = mongo.db.smoothie_recipes
     ingredients = request.form.get('ingredients')
     ingredient_list = ingredients.splitlines()
     keyword_search = request.form.get('keyword_search')
@@ -57,7 +54,7 @@ def insert_smoothie():
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
-    categories=mongo.db.categories.find())
+    categories=categories.find())
 
 
 @app.route('/add_category')
@@ -67,19 +64,19 @@ def add_category():
 
 @app.route('/delete_category/<category_id>', methods=['POST'])
 def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
-    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+    category=categories.find_one({'_id': ObjectId(category_id)}))
 
 
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
-    mongo.db.categories.update(
+    categories.update(
         {'_id': ObjectId(category_id)},
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
@@ -88,21 +85,27 @@ def update_category(category_id):
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
     category_doc = {'category_name': request.form.get('category_name').lower()}
-    mongo.db.categories.insert_one(category_doc)
+    categories.insert_one(category_doc)
     return redirect(url_for('get_categories'))
 
 
-# START CREDIT TO SHANE MUIRHEAD ON SLACK
+# CREDIT TO SHANE MUIRHEAD ON SLACK FOR UPVOTE HELP
 @app.route('/upvote/<smoothie_recipes_id>')
 def upvote(smoothie_recipes_id):
-    mongo.db.smoothie_recipes.find_one_and_update(
+    smoothie_recipes.find_one_and_update(
         {'_id': ObjectId(smoothie_recipes_id)},
         {'$inc': {'upvotes': int(1)}}
     )
     return redirect(url_for('get_smoothies')) 
-# END CREDIT TO SHANE MUIRHEAD ON SLACK
 
 
+@app.route('/search')
+def search():
+    smoothie_recipes.create_index([("$**", 'text')])
+    smoothie_recipes.find({"$text": {"$search": str('banana')}})
+    return render_template("search-results.html", smoothie_recipes=smoothie_recipes)
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
