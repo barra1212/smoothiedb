d15482e7ee77b0e881a3a209ca8f3cc3c469043b
barrycunningham <cunnbarry@gmail.com>
2019-04-06 16:50:58 +0000

    Documentation complete, code commenting complete, debug set to False, push to Heroku

--- smoothiedb.py
+++ smoothiedb.py
@@ -33,41 +33,6 @@ def add_smoothie():
     categories=categories.find())
     
 
-# GO TO EDIT SMOOTHIE RECIPE PAGE
-@app.route('/edit_smoothie/<smoothie_recipes_id>')
-def edit_smoothie(smoothie_recipes_id):
-    the_smoothie = mongo.db.smoothie_recipes.find_one({"_id": ObjectId(smoothie_recipes_id)})
-    all_categories = categories.find()
-    return render_template('edit_smoothie.html', smoothie=the_smoothie, categories=all_categories)
-
-
-# UPDATE SMOOTHIE RECIPE AND WRITE TO THE DATABASE
-@app.route('/update_smoothie/<smoothie_recipes_id>', methods=['POST'])
-def update_smoothie(smoothie_recipes_id):
-    smoothie_recipes=mongo.db.smoothie_recipes
-    ingredients = request.form.get('ingredients')
-    ingredient_list = ingredients.splitlines()
-    keyword_search = request.form.get('keyword_search').lower()
-    keyword_search_list = keyword_search.splitlines()
-    while True:
-        if "UHBq531pl," == request.form.get('secret'):
-            smoothie_recipes.update( {'_id': ObjectId(smoothie_recipes_id)},
-            {
-                "smoothie_name": request.form.get('smoothie_name'),
-                "category_name": request.form.get('category_name'),
-                "description": request.form.get('description'),
-                "ingredients": ingredient_list,
-                "method": request.form.get('method'),
-                "calories": request.form.get('calories'),
-                "keyword_search": keyword_search_list,
-                "secret": request.form.get('secret'),
-                "upvotes": int(0),
-            })
-            return redirect(url_for('get_smoothies'))
-        else:
-            return render_template('error.html')
-
-
 # WRITE NEW SMOOTHIE RECIPE TO THE DATABASE
 @app.route('/insert_smoothie', methods=['POST'])
 def insert_smoothie():
@@ -88,6 +53,41 @@ def insert_smoothie():
     }
     smoothie_recipes.insert_one(smoothie)
     return redirect(url_for('get_smoothies'))
+
+
+# GO TO EDIT SMOOTHIE RECIPE PAGE
+@app.route('/edit_smoothie/<smoothie_recipes_id>')
+def edit_smoothie(smoothie_recipes_id):
+    the_smoothie = mongo.db.smoothie_recipes.find_one({"_id": ObjectId(smoothie_recipes_id)})
+    all_categories = categories.find()
+    return render_template('edit_smoothie.html', smoothie=the_smoothie, categories=all_categories)
+
+
+# UPDATE SMOOTHIE RECIPE AND WRITE TO THE DATABASE
+@app.route('/update_smoothie/<smoothie_recipes_id>', methods=['POST'])
+def update_smoothie(smoothie_recipes_id):
+    smoothie_recipes=mongo.db.smoothie_recipes
+    ingredients = request.form.get('ingredients').pretty()
+    ingredient_list = ingredients.splitlines()
+    keyword_search = request.form.get('keyword_search').lower()
+    keyword_search_list = keyword_search.splitlines()
+    while True:
+        if "UHBq531pl," == request.form.get('secret'):
+            smoothie_recipes.update( {'_id': ObjectId(smoothie_recipes_id)},
+            {
+                "smoothie_name": request.form.get('smoothie_name'),
+                "category_name": request.form.get('category_name'),
+                "description": request.form.get('description'),
+                "ingredients": ingredient_list,
+                "method": request.form.get('method'),
+                "calories": request.form.get('calories'),
+                "keyword_search": keyword_search_list,
+                "secret": request.form.get('secret'),
+                "upvotes": int(0),
+            })
+            return redirect(url_for('get_smoothies'))
+        else:
+            return render_template('error.html')
 
 
 # DELETE SMOOTHIE - UNUSED ON LIVE FRONT END
@@ -126,9 +126,9 @@ def delete_category(category_id):
 
 
 # EDIT A CATEGORY - UNUSED ON LIVE FRONT END
-@app.route('/edit_category/<category_id>')
+@app.route('/edit_category/<category_id>', methods=['POST'])
 def edit_category(category_id):
-    return render_template('editcategory.html',
+    return render_template('edit_category.html',
     category=categories.find_one({'_id': ObjectId(category_id)}))
 
 
@@ -169,4 +169,4 @@ def error():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
-        debug=True)
+        debug=False)
